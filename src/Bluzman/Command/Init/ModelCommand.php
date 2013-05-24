16c63a95502468b5d524962d181cf13d23239e6f
Pavel Machekhin <pavel.machekhin@gmail.com>
2013-05-24 20:51:30 +0300

    Refactored ModelCommand, added confirmation for table name generated automatically from model name

--- src/Bluzman/Command/Init/ModelCommand.php
+++ src/Bluzman/Command/Init/ModelCommand.php
@@ -28,17 +28,17 @@ class ModelCommand extends AbstractCommand
     protected function configure()
     {
         $this
-            ->setName('init:model')
-            ->setDescription('Initialize a new model')
+            ->setName("init:model")
+            ->setDescription("Initialize a new model")
             ->addArgument(
-                'name',
+                "name",
                 InputArgument::OPTIONAL,
-                'The name of model. In case of empty `table` argument it is also a name of database table.'
+                "The name of model. In case of empty `table` argument it is also a name of database table."
             )
             ->addArgument(
-                'table',
+                "table",
                 InputArgument::OPTIONAL,
-                'The name of DB table.'
+                "The name of DB table."
             );
     }
 
@@ -49,41 +49,24 @@ class ModelCommand extends AbstractCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Running "init:model" command</info>');
+        $output->writeln("<info>Running \"init:model\" command</info>");
 
         try {
-            $name = $input->getArgument('name');
-            $table = $input->getArgument('table');
+            $this->verify($input, $output);
 
-            // `name` is required argument
-            if (empty($name)) {
-                // command should be executed only in interactive mode
-                if (!$input->isInteractive()) {
-                    throw new \RuntimeException('Command should be executed in interactive mode.');
-                }
-
-                $output->writeln('');
-                $name = $this->askName($output);
-            }
-
-            if (empty($table)) {
-                $table = $name;
-            }
-
-            $name = strtolower($name);
-            $table = strtolower($table);
+            $name = $input->getArgument("name");
+            $table = $input->getArgument("table");
 
             $this->generate($name, $table, $input, $output);
 
-            $output->writeln('');
-            $output->writeln('Model <info>"' . $name . '"</info> has been successfully created</info>.');
+            $output->writeln("\nModel <info>\"" . $name . "\"</info> has been successfully created</info>.");
 
         } catch (\LogicException $e) {
             throw new \RuntimeException($e->getMessage());
         } catch (\RuntimeException $e) {
             throw new \RuntimeException($e->getMessage());
         } catch (\Exception $e) {
-            throw new \RuntimeException('Some error occurred.');
+            throw new \RuntimeException("Some error occurred.");
         }
     }
 
@@ -91,40 +74,91 @@ class ModelCommand extends AbstractCommand
      * @param OutputInterface $output
      * @return mixed
      */
-    protected function askName(OutputInterface $output)
+    protected function askModelName(InputInterface $input, OutputInterface $output)
     {
-        $dialog = $this->getHelperSet()->get('dialog');
+        $dialog = $this->getHelperSet()->get("dialog");
 
         // ask user enter a valid name of new model
         return $dialog->askAndValidate(
             $output,
-            "<question>Please enter the name of the model:</question> \n> ",
-            function ($name) use ($output, $dialog) {
-                if (empty($name)) {
-                    $output->writeln('<error>ERROR: Please enter a correct name of the model</error>');
-
-                    return $this->askName($output);
-                } else {
-                    return $name;
-                }
+            "\n<question>Please enter the name of the model:</question> \n> ",
+            function ($name) use ($input, $output, $dialog) {
+                return $this->validateModelName($name, $input, $output);
             },
             true
         );
     }
 
     /**
-     * Get table primary key
-     *
      * @param $name
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    protected function validateModelName($name, InputInterface $input, OutputInterface $output)
+    {
+        if (empty($name)) {
+            $output->writeln("\n<error>ERROR: Please enter a correct name of the model</error>");
+
+            return $this->askModelName($input, $output);
+        }
+
+        $input->setArgument("name", $name);
+
+        return $name;
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    protected function askTableName(InputInterface $input, OutputInterface $output)
+    {
+        $dialog = $this->getHelperSet()->get("dialog");
+
+        // ask user enter a valid name of new model
+        return $dialog->askAndValidate(
+            $output,
+            "\n<question>Please enter correct name of the table:</question> \n> ",
+            function ($table) use ($input, $output, $dialog) {
+                return $this->validateTable($table, $input, $output);
+            },
+            true
+        );
+    }
+
+    /**
+     * @param $table
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    protected function validateTable($table, InputInterface $input, OutputInterface $output)
+    {
+        if (empty($table)) {
+            $output->writeln("\n<error>ERROR: Please enter a correct name of the table</error>");
+
+            return $this->askTableName($input, $output);
+        }
+
+        $input->setArgument("table", $table);
+
+        return $table;
+    }
+
+    /**
+     * Get primary key of table
+     *
+     * @param $table
      * @return array
      */
-    protected function getPrimaryKey($name)
+    protected function getPrimaryKey($table)
     {
         $dbh = $this->getApplication()->getDbConnection();
 
-        $q = $dbh->prepare("DESCRIBE $name");
+        $dbh->prepare("DESCRIBE $table");
 
-        $q = $dbh->prepare("SHOW KEYS FROM $name WHERE Key_name = 'PRIMARY'");
+        $q = $dbh->prepare('SHOW KEYS FROM $name WHERE Key_name = "PRIMARY"');
 
         $q->execute();
         $keys = $q->fetchAll(\PDO::FETCH_ASSOC);
@@ -132,7 +166,7 @@ class ModelCommand extends AbstractCommand
         $primaryKeys = array();
 
         foreach ($keys as $key) {
-            $primaryKeys[] = $key['Column_name'];
+            $primaryKeys[] = $key["Column_name"];
         }
 
         return $primaryKeys;
@@ -153,11 +187,11 @@ class ModelCommand extends AbstractCommand
         $q->execute();
 
         foreach($q->fetchAll(\PDO::FETCH_ASSOC) as $column) {
-            $type = preg_replace('/\(.*/s', '', $column['Type']);
+            $type = preg_replace("/\(.*/s", "", $column["Type"]);
 
             $columns[] = array(
-                'name' => $column['Field'],
-                'type' => $type
+                "name" => $column["Field"],
+                "type" => $type
             );
         };
 
@@ -180,62 +214,62 @@ class ModelCommand extends AbstractCommand
         try {
             // generate table
             $arguments = array(
-                'Table.php',
+                "Table.php",
                 $this->getPath($modelName),
                 Generator\Generator::ENTITY_TYPE_MODEL_TABLE,
                 array(
-                    'name' => ucfirst($modelName),
-                    'table' => $table,
-                    'primaryKey' => $primaryKey
+                    "name" => $modelName,
+                    "table" => $table,
+                    "primaryKey" => $primaryKey
                 )
             );
 
-            call_user_func_array(array($generator, 'generateTemplate'), $arguments);
+            call_user_func_array(array($generator, "generateTemplate"), $arguments);
 
         } catch (Generator\Template\Exception\AlreadyExistsException $e) {
-            $dialog = $this->getHelperSet()->get('dialog');
+            $dialog = $this->getHelperSet()->get("dialog");
 
             $result = $dialog->askConfirmation(
                 $output,
-                "<question>Model " . $e->getMessage() . " would be overwritten. y/N?:</question>\n> ",
+                "\n<question>Model " . $e->getMessage() . " would be overwritten. y/N?:</question>\n> ",
                 false
             );
 
             if ($result) {
                 $arguments[] = true; // rewrite argument
 
-                call_user_func_array(array($generator, 'generateTemplate'), $arguments);
+                call_user_func_array(array($generator, "generateTemplate"), $arguments);
             }
         }
 
         try {
             // generate row
             $arguments = array(
-                'Row.php',
+                "Row.php",
                 $this->getPath($modelName),
                 Generator\Generator::ENTITY_TYPE_MODEL_ROW,
                 array(
-                    'name' => ucfirst($modelName),
-                    'table' => $table,
-                    'columns' => $columns
+                    "name" => $modelName,
+                    "table" => $table,
+                    "columns" => $columns
                 )
             );
 
-            call_user_func_array(array($generator, 'generateTemplate'), $arguments);
+            call_user_func_array(array($generator, "generateTemplate"), $arguments);
 
         } catch (Generator\Template\Exception\AlreadyExistsException $e) {
-            $dialog = $this->getHelperSet()->get('dialog');
+            $dialog = $this->getHelperSet()->get("dialog");
 
             $result = $dialog->askConfirmation(
                 $output,
-                "<question>Model " . $e->getMessage() . " would be overwritten. y/N?:</question>\n> ",
+                "\n<question>Model " . $e->getMessage() . " would be overwritten. y/N?:</question>\n> ",
                 false
             );
 
             if ($result) {
                 $arguments[] = true; // rewrite argument
 
-                call_user_func_array(array($generator, 'generateTemplate'), $arguments);
+                call_user_func_array(array($generator, "generateTemplate"), $arguments);
             }
         }
     }
@@ -249,15 +283,15 @@ class ModelCommand extends AbstractCommand
     protected function getPath($modelName)
     {
         $modelPath = $this->getApplication()->getPath() . DIRECTORY_SEPARATOR
-            . 'application' . DIRECTORY_SEPARATOR
-            . 'models' . DIRECTORY_SEPARATOR
-            . 'Application';
+            . "application" . DIRECTORY_SEPARATOR
+            . "models" . DIRECTORY_SEPARATOR
+            . "Application";
 
         if (!is_dir($modelPath)) {
             mkdir($modelPath, 0755);
         }
 
-        $path = $modelPath . DIRECTORY_SEPARATOR . ucfirst($modelName);
+        $path = $modelPath . DIRECTORY_SEPARATOR . $modelName;
 
         if (!is_dir($path)) {
             mkdir($path, 0755);
@@ -267,12 +301,51 @@ class ModelCommand extends AbstractCommand
     }
 
     /**
-     * @todo
-     *
-     * @return bool
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @throws \RuntimeException
      */
     public function verify(InputInterface $input, OutputInterface $output)
     {
-        return true;
+        // command should be executed only in interactive mode
+        if (!$input->isInteractive()) {
+            throw new \RuntimeException("Command should be executed in interactive mode.");
+        }
+
+        $name = $input->getArgument("name");
+
+        // `name` is required argument
+        if (empty($name)) {
+            $this->askModelName($input, $output);
+        } else {
+            $this->validateModelName($name, $input, $output);
+        }
+
+        $name = $input->getArgument("name");
+        $table = $input->getArgument("table");
+
+        if (empty($table)) {
+            $table = strtolower($name);
+            $dialog = $this->getHelperSet()->get("dialog");
+
+            $result = $dialog->askConfirmation(
+                $output,
+                "\n<question>The name of database table would be \"" . $table . "\". Y/n?:</question>\n> ",
+                true
+            );
+
+            if (!$result) {
+                $this->askTableName($input, $output);
+            } else {
+                $input->setArgument('table', $table);
+            }
+        }
+
+        $table = $input->getArgument("table");
+
+        $this->validateTable($table, $input, $output);
+
+        $input->setArgument("name", ucfirst($name));
+        $input->setArgument("table", $table);
     }
 }
