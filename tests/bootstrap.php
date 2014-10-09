fa27a948fbccbf3cabf44d9da299f4e80ea4e772
Pavel Machekhin <pavel.machekhin@gmail.com>
2014-10-09 18:45:15 +0300

    Switched to PSR-4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,6 @@ define('DS', DIRECTORY_SEPARATOR);
 // Paths
 define('PATH_ROOT', realpath(dirname(__FILE__). DS . '..' . DS));
 define('PATH_VENDOR', PATH_ROOT . DS . 'vendor');
-define('PATH_FIXTURES', PATH_ROOT . DS . 'tests' . DS . 'Bluzman' . DS . 'Tests' . DS . 'fixtures');
 define('PATH_TMP', PATH_ROOT . DS . 'tests' . DS . 'Bluzman' . DS . 'Tests' . DS . 'tmp');
 
 // register composer autoloader
