5f7bd9a8a9140eda312992e89b7650d71a83d1d4
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-04-10 15:14:11 +0300

    Removed optional unused code Moved initialization of ENV to `bin/bluzman`

--- src/Application/Application.php
+++ src/Application/Application.php
@@ -50,31 +50,6 @@ class Application extends Console\Application
     }
 
     /**
-     * Removed some commands from default input definition.
-     *
-     * @return InputDefinition An InputDefinition instance
-     */
-    protected function getDefaultInputDefinition()
-    {
-        return new InputDefinition(
-            [
-                new InputArgument('command', InputArgument::REQUIRED, 'The command to execute'),
-                new InputOption(
-                    '--env',
-                    '-e',
-                    InputOption::VALUE_REQUIRED,
-                    'The environment to be used',
-                    getenv('BLUZ_ENV') ?: 'production'
-                ),
-                new InputOption('--help', '-h', InputOption::VALUE_NONE, 'Display this help message'),
-                new InputOption('--quiet', '-q', InputOption::VALUE_NONE, 'Do not output any message'),
-                new InputOption('--verbose', '-v', InputOption::VALUE_NONE, 'Increase verbosity of messages'),
-                new InputOption('--version', '-V', InputOption::VALUE_NONE, 'Display this application version')
-            ]
-        );
-    }
-
-    /**
      * Register Bluzman commands
      *
      * @todo Find a way to do this automatically or move it to /bin/bluzman
